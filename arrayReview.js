6deb57f0f1bf614af63bc729e5a4ddeea1306f8b
Jacob Robertson <jwrob1026@gmail.com>
2015-04-08 16:00:13 -0600

    update on review #1

--- arrayReview.js
+++ arrayReview.js
@@ -1,8 +1,13 @@
-var threeItems = [1,2,3];
+var threeItems = [1, 2, 3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+//code here
+
+var last = function(arr) {
+  return arr[arr.length - 1];
+};
+last(threeItems);
 
 
 
@@ -11,33 +16,48 @@ var threeItems = [1,2,3];
 
 
 //Loop through evenArray removing all values that aren't even 
-var evenArray = [1,2,3,6,22,98,45,23,22,12];
+var evenArray = [1, 2, 3, 6, 22, 98, 45, 23, 22, 12];
 
-  //code here
-
-
+var newArr = function(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+};
+newArr(evenArray);
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false
+/*below you're given a function that will return a random number between 0 and 30 and an array full or numbers 'randomArray'. Your job is to write a function that will get
+// a random number, then loop through the array to see if that random number is in the array. If it is, alert true, if it's not, alert false*/
 var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
-var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
+var randomArray = [0, 3, 4, 5, 6, 7, 9, 14, 17, 24, 25, 26, 29, 30];
 
-  //code here
+//code here
+
+var solution = function(arr) {
+  return arr.indexOf(getRandomArbitrary()) >= 0;
+};
+solution(randomArray);
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you created your copy correctly.
-var first = [1,2,3,4,5];
+//Create a copy of first and save it into second. Then, add 6 and 7 to the end of second. When you run this, first should be just [1,2,3,4,5] and second will be [1,2,3,4,5,6,7] if you 
+//created your copy correctly.
+var first = [1, 2, 3, 4, 5];
 var second;
 
-  //code here
+second = first.slice();
+second.push(6, 7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -50,8 +70,20 @@ alert(second) //[1,2,3,4,5,6,7];
 
 //write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
 var sentence = "Dev Mountain is the best"
+var longest = function(string) {
+  var str = string.split(" ");
+  var long = 0;
+  var word = null;
+  for (var i = 0; i < str.length - 1; i++) {
+    if (long < str[i].length) {
+      long = str[i].length;
+      word = str[i];
+    }
+  }
+  return word;
+};
 
-  //code here
+//code here
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -60,9 +92,9 @@ var sentence = "Dev Mountain is the best"
 
 //write a function called capitalize that takes in the myPoem variable and capitalizes every word 
 var myPoem = 'What is a jQuery but a misunderstood object?'
-//What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
+  //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
 
-  //code here
+//code here
 
 
 
